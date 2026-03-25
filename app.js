376ea05e3536aa0e36cca2337493364d81696104
merampe0892 <moi.orionix92@proton.me>
2026-03-24 21:24:01 -0500

    almost there

--- app.js
+++ app.js
@@ -1,32 +1,51 @@
-let url = `https://pokeapi.co/api/v2/pokemon/1/`;
+const grid = document.querySelector('.pokemon__grid');
+
+function onSearchChange(event) {
+    console.log(event.target.value)
+}
+
 async function fetchKantoPokemon() {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+  try {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=18');
     const allPokemon = await response.json();
-    console.log(allPokemon)
 
-    allPokemon.results.map((pokemon) => 
-        fetch(pokemon.url).then(res => res.json()).then(allPokemon => {
-        console.log(allPokemon)
-        renderPokemon(allPokemon);
-    }))
+    const detailPromises = allPokemon.results.map(async (pokemon) => {
+      const res = await fetch(pokemon.url);
+      const pokemonData = await res.json();
+      return pokemonData;
+    });
+
+    const pokemonDetails = await Promise.all(detailPromises);
+
+    grid.innerHTML = '';
+    pokemonDetails.forEach((pokemonData) => {
+      renderPokemon(pokemonData);
+    });
+  } catch (error) {
+    console.error('Error fetching Kanto Pokémon:', error);
+  }
 }
 
-function renderPokemon(allPokemon) {
-    const allPokemonContainer = document.querySelector('pokemon__grid');
-    allPokemonContainer.innerHTML = (
-        (pokemon) =>
-        `<div class="pokemon-card">
-            <img src="${allPokemon.sprites.front_default}" alt="Pokémon sprite">
-            <h4>${allPokemon.name}</h4>
-            <p>#${allPokemon.id}</p>
-            <ul>
-                <li>${allPokemon.types[0].type.name}</li>
-                <li>${allPokemon.types[1] ? allPokemon.types[1].type.name : 'None'}</li>
-            </ul>
-        </div>`).join('');
-}
+function renderPokemon(pokemon) {
+  const card = document.createElement('div');
+  card.classList.add('pokemon-card');
 
+  card.innerHTML = `
+    <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+    <h4>${pokemon.name}</h4>
+    <p>#${pokemon.id}</p>
+    <ul>
+      <li>${pokemon.types[0].type.name}</li>
+      <li>${pokemon.types[1] ? pokemon.types[1].type.name : 'None'}</li>
+    </ul>
+  `;
+
+  grid.appendChild(card);
+}
 
 fetchKantoPokemon();
 
 
+function showPokemon(pokemonName) {
+    console.log(localStorage.setItem("pokemonName", pokemonName))
+}
